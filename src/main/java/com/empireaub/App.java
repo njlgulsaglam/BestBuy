86b2132bd7a3c622796c4460cab4a66d4d369841
nejlagulsaglam <njlgulsaglam@gmail.com>
2018-06-26 00:05:07 -0400

    BestBuy maven project

---
+++ src/main/java/com/empireaub/App.java
@@ -0,0 +1,13 @@
+package com.empireaub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/empireaub/BestBuy.java
@@ -0,0 +1,17 @@
+package com.empireaub;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BestBuy {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.bestbuy.com");
+	}
+
+}
